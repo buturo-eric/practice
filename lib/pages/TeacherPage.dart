ab9cc9a77bec8a563ba47a704d03fcfa156d9745
buturo-eric <ericbuturo1@gmail.com>
2024-03-14 20:56:47 +0200

    first commit

--- lib/pages/TeacherPage.dart
+++ lib/pages/TeacherPage.dart
@@ -102,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
   @override
-  void initState() {
+  void initState() { 
     // Initialize the database service with a unique ID
     databaseService = DatabaseService(uid: Uuid().v4());
 
